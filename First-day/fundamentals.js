edb49397177a4f836197662e2efd903e36005665
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2024-12-12 21:04:42 +0200

    last questions

--- First-day/fundamentals.js
+++ First-day/fundamentals.js
@@ -326,3 +326,20 @@ Expected
 myFunction(26.1379)
 Expected
 26.14*/
+
+// 20.Write a function that takes a number (a) as argument. Split a into its individual digits and return them in an array. Hint: you might want to change the type of the number for the splitting
+function myFunction(a) {
+  return a.toString().split("").map(Number);
+}
+/*Test Cases:
+Attempted: 182597
+Solved: 32601
+myFunction(10)
+Expected
+[1,0]
+myFunction(931)
+Expected
+[9,3,1]
+myFunction(193278)
+Expected
+[1,9,3,2,7,8]*/
